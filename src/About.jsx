0c5b7f028e1ebb21c7fd443128a213b239894d7d
sumedhjv <68630892+kabowabo@users.noreply.github.com>
2024-08-24 13:29:37 -0700

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -8,16 +8,7 @@ function About(){
         <div><p className="ctgr">about (00)</p></div>
         <br/>
         <ul>
-            <li className="rowie"><p>duomo faces duomo in the duomo crowd;</p></li>
-            <li className="rowie"><p>duomo petals on duomo a wet, duomo black bough.</p></li>
-            <li className="rowie"><p>duomo whispers of duomo these fleeting duomo visages,</p></li>
-            <li className="rowie"><p>duomo in the murmurs duomo of duomo a timeless duomo crowd.</p></li>
-            <li className="rowie"><p>duomo echoes linger duomo in duomo the silent duomo air;</p></li>
-            <li className="rowie"><p>duomo whispers cascade duomo like duomo delicate petals duomo rare.</p></li>
-            <li className="rowie"><p>duomo in the shadows duomo of duomo faces unseen,</p></li>
-            <li className="rowie"><p>duomo essence lingers duomo in duomo spaces between.</p></li>
-            <li className="rowie"><p>duomo, the heart's duomo silent duomo vow,</p></li>
-            <li className="rowie"><p>duomo woven through duomo moments duomo here and now.</p></li>
+            <li className="rowie"><p> l u c k y  d u c k;</p></li>
          </ul>
     </div>
 </div> 
