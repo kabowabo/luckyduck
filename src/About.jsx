6cb7dafb5841d5c7cdd2425a1948ef8722fd2536
sumedhjv <68630892+kabowabo@users.noreply.github.com>
2024-08-24 13:30:29 -0700

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -8,7 +8,7 @@ function About(){
         <div><p className="ctgr">about (00)</p></div>
         <br/>
         <ul>
-            <li className="rowie"><p> l u c k y  d u c k;</p></li>
+            <li className="rowie"><p> l u c k y  d u c k</p></li>
          </ul>
     </div>
 </div> 
