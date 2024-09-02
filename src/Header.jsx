daba00c65a5421c2bd74513083d757aee36fccb0
sumedhjv <68630892+kabowabo@users.noreply.github.com>
2024-09-02 11:43:49 -0700

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,29 +1,50 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
-import cartlogo from "./assets/cartlogo.png"
-import wavylogo from "./assets/wavyluckyduck.gif"
+import cartlogo from "./assets/cartlogo.png";
+import wavylogo from "./assets/wavyluckyduck.gif";
 
-function Header(){
+function Header() {
   const navigate = useNavigate();
   
   const handleClick = (path) => {
-      navigate(path);
+    navigate(path);
   };
-    return(
-        <div>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-      <a className="nav-link navL1" href="javascript:void(0)" onClick={() => handleClick('/')}>
-        <svg width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
-          <image href={wavylogo} width="100" height="100" />
-        </svg>
-      </a>
-      <a className="nav-link navL2" href="javascript:void(0)" onClick={() => handleClick('/cart')}>
-        <img src={cartlogo}/><span className="cartnum"><p>{`(0)`}</p></span>
-      </a>
-      </div>
-  </nav>
+
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          {/* Home link with logo */}
+          <a 
+            className="nav-link navL1" 
+            href="javascript:void(0)" 
+            onClick={() => handleClick('/')}
+          >
+            <svg 
+              width="1120px" 
+              height="630px" 
+              viewBox="0 0 560 315" 
+              preserveAspectRatio="xMidYMid meet"
+            >
+              <image href={wavylogo} width="560" height="315" />
+            </svg>
+          </a>
+
+          {/* Cart link */}
+          <a 
+            className="nav-link navL2" 
+            href="javascript:void(0)" 
+            onClick={() => handleClick('/cart')}
+          >
+            <img src={cartlogo} alt="Cart" />
+            <span className="cartnum">
+              <p>(0)</p>
+            </span>
+          </a>
         </div>
-    );
+      </nav>
+    </header>
+  );
 }
+
 export default Header;
